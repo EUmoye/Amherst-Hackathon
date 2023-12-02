96d1bc71447995d3a2f3d50876df16460b27559e
abigel-asteraye <aster22a@mtholyoke.edu>
2023-12-02 15:48:22 -0500

    summarizer now working

--- popup.js
+++ popup.js
@@ -1,11 +1,143 @@
 window.addEventListener('DOMContentLoaded', function () {
     // Example: Change the background color of the body
     document.body.style.backgroundColor = 'lightblue';
+    document.getElementById('summarizeButton').addEventListener('click', 
+        chrome.tabs.executeScript({
+            code: 'window.getSelection().toString()'
+        }, function (selection) {
+            var selectedText = selection && selection.length > 0 ? selection[0] : null;
+            getSummaryText(selectedText);
+        })
+    );
 
-    // Example: Summarize the current webpage
-    function summarizePage() {
-        // Your code to summarize the page
+    // Dynamically attach the click event for the Text to Speech button
+    document.getElementById('textToSpeechButton').addEventListener('click', function () {
+        chrome.tabs.executeScript({
+            code: 'window.getSelection().toString();'
+        }, function (selection) {
+            var selectedText = selection && selection.length > 0 ? selection[0] : null;
+            textToSpeech(selectedText);
+        });
+    });
+    
+
+
+    const charLimit = 100000000;
+    const apiKey = "uEewNRoSmbWPaz9zX2JcxhjVrOcBk5npl99k0jiG";
+
+    // Display the text at the top of the page
+    function display(text) {
+    print("This display is invoked");
+    header = document.createElement("div");
+    header.style.backgroundColor = "white";
+    header.style.padding = "5px";
+
+
+    // Write the text with a bit of styling and add it to the header
+    tldr = document.createElement("p");
+    tldr.textContent = text;
+    tldr.style.margin = "10px 100px";
+    tldr.style.fontSize = "medium";
+    tldr.style.color = "black";
+    tldr.style.textAlign = "left";
+    tldr.style.fontFamily = "Verdana, Geneva, sans-serif";
+    header.appendChild(tldr);
+
+
+    // Insert the header immediately before the HTML body
+    document.body.parentNode.insertBefore(header, document.body);
+}
+
+    function summarize(text) {
+        console.log(text)
+        print("This summarize is invoked");
+
+
+      options = {
+          "method": "POST",
+          "headers": {
+              "Accept": "application/json",
+              "Content-type": "application/json",
+              "Authorization": "Bearer " + apiKey,
+              "Request-Source": "sandbox-condense"
+          },
+          "body": JSON.stringify({
+              "length": "long",
+              "format": "auto",
+              "model": "summarize-xlarge",
+              "extractiveness": "low",
+              "temperature": 0.1,
+              "text": text,
+              // We tell the model that it's summarizing a webpage
+              "additional_command": "of this webpage"
+          })
+      };
+
+      fetch('https://api.cohere.ai/v1/summarize', options)
+      .then((response) => response.json())
+      .then((response) => {
+          if (response.summary === undefined) {
+              // If there's no summary in the endpoint's response,
+              // display whatever error message it returned
+              display("There was an error: " + response.message);
+          } else {
+              // Otherwise, display the summary
+              display("tl;dr: " + response.summary);
+          }
+      });
     }
+
+    // Returns true if the given element isn't visible on the page
+function isHidden(el) {
+    var style = window.getComputedStyle(el);
+    return ((style.display === 'none') || (style.visibility === 'hidden'))
+  }
+  
+  
+  // Returns only the visible text from the page
+  function getVisibleText() {
+    var body = document.querySelector('body')
+    if (document.querySelector('#content')) {
+        body = document.querySelector('#content');
+    }
+    if (document.main) {
+        body = document.querySelector('main');
+    }
+    var allTags = body.getElementsByTagName('*');
+  
+  
+    let visibleText = [];
+    var nChars = 0;
+    // Select all visible text in the body, up to charLimit
+    for (var i = 0, max = allTags.length; i < max; i++) {
+        var elem = allTags[i];
+        if (!isHidden(elem)) {
+  
+  
+            var textNodes = Array.from(elem.childNodes).filter(function (node) {
+                return node.nodeType === Node.TEXT_NODE && node.nodeValue.trim() !== '';
+            });
+  
+  
+            if (textNodes.length === 0) {
+                continue;
+            }
+  
+  
+            var text = textNodes[0].nodeValue;
+            nChars += text.length + 1; // for newline
+            if (nChars < charLimit) {
+                visibleText.push(text);
+            } else {
+                break
+            }
+        }
+    }
+    // Separate all the text elements with a newline
+    return visibleText.join('\n');
+  }
+
+
 
     // Example: Change the theme
     function changeTheme() {
@@ -27,44 +159,47 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    function getSummaryText(selectedText) {
+        if (selectedText) {
+            summarize(selectedText);
+        } else {
+            chrome.tabs.executeScript({
+                code: 'document.body.innerText'
+            }, function (pageContent) {
+                summarize(pageContent[0]);
+            });
+        }
+    }
+
     function speakText(text) {
         var synth = window.speechSynthesis;
         var utterance = new SpeechSynthesisUtterance(text);
         synth.speak(utterance);
     }
 
-    // Attach event listeners for buttons
-    document.getElementById('summarizeButton').addEventListener('click', summarizePage);
 
-    // Dynamically attach the click event for the Text to Speech button
-    document.getElementById('textToSpeechButton').addEventListener('click', function () {
-        chrome.tabs.executeScript({
-            code: 'window.getSelection().toString();'
-        }, function (selection) {
-            var selectedText = selection && selection.length > 0 ? selection[0] : null;
-            textToSpeech(selectedText);
-        });
-    });
+//     // Attempt to summarize the page
+//     function main() {
 
-    // Example: Attach event listener for the "Summarize Text" button
-    document.getElementById('summarizeTextButton').addEventListener('click', function () {
-        summarizeText();
-    });
-});
 
-function summarizeText() {
-    // Simulated summarized text, replace this with your actual summarized text
-    var summarizedText = "Summarized text...";
+//     const apiKey = "uEewNRoSmbWPaz9zX2JcxhjVrOcBk5npl99k0jiG";
+  
+  
+  
+  
+//             // If there is a key, we can use it to summarize the page
+//             const truncatedVisibleText = getSummaryText()
+//             // During the dev process, it's helpful to be able to see exactly what
+//             // text is being summarized
+//             console.log(truncatedVisibleText);
+  
+  
+//             summarize(truncatedVisibleText);
+//   }
+  
+  
+  // This calls main() when pages are loaded.
+  });
 
-    // Get the textSummaryContainer
-    var textSummaryContainer = document.getElementById("summarization-container");
 
-    // Show the text summary container
-    textSummaryContainer.style.display = "block";
-
-    // Get the textSummary element
-    var textSummaryElement = document.getElementById("textsummary");
-
-    // Set the content to display the summarized text
-    textSummaryElement.innerHTML = "<p>" + summarizedText + "</p>";
-}
+    
