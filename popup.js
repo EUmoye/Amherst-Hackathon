1b8390154ee91f0d8dea68e986155908e6656cfe
Genuama2 <116984032+Genuama2@users.noreply.github.com>
2023-12-01 20:28:01 -0500

    Creating new files for project

---
+++ popup.js
@@ -0,0 +1,27 @@
+// Example JavaScript code for popup.js
+document.addEventListener('DOMContentLoaded', function () {
+    // Your code to execute when the DOM is ready
+
+    // Example: Change the background color of the body
+    document.body.style.backgroundColor = 'lightblue';
+
+    // Example: Summarize the current webpage
+    function summarizePage() {
+        // Your code to summarize the page
+    }
+
+    // Example: Change the theme
+    function changeTheme() {
+        // Your code to change the theme
+    }
+
+    // Example: Text-to-speech
+    function textToSpeech() {
+        // Your code for text-to-speech
+    }
+
+    // Attach event listeners or call functions based on user interactions
+    document.getElementById('summarizeButton').addEventListener('click', summarizePage);
+    document.getElementById('themeButton').addEventListener('click', changeTheme);
+    document.getElementById('textToSpeechButton').addEventListener('click', textToSpeech);
+});
